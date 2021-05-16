b970aaf8c055a31e0e1e47dc4ea9efd7992c478a
Patrick16 <mezolonder@gmail.com>
2021-05-16 19:36:12 +0300

    classwork

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import Header from '../../components/Header';
+import Button from '../../components/Button';
+import Layout from '../../components/Layout';
 
 import s from './Home.module.scss';
+import Parallax from '../../components/Parallax';
 
 const HomePage = () => {
   return (
     <div className={s.root}>
       <Header />
+      <Layout className={s.contentWrap}>
+        <div className={s.contentText}>
+          <h1>
+            <b>Find</b> all your favourite <b>Pokemon</b>
+          </h1>
+          <p>You can know the type of your Pokemon, its strengths, disadvantages and abilities</p>
+          <Button onClick={() => console.log('Button pushed')}>See pokemons</Button>
+        </div>
+        <div className={s.contentParallax}>
+          <Parallax />
+        </div>
+      </Layout>
     </div>
   );
 };
